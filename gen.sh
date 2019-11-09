3ea016eb759e63d3b7e5a67cc09c93c0a710a09c
zixuanweeei <zixuan.wei@intel.com>
2019-11-09 17:37:52 +0800

    clean

--- gen.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-as -o kernel_avx512.out kernel_avx512.s
-/opt/intel/bin/icc -o start_avx512_a.out start_avx512_a.c kernel_avx512.out -O2
-/opt/intel/bin/icc -o start_avx512.out start_avx512.c -O2
--- run.sh
+++ run.sh
@@ -3,7 +3,4 @@ if [ "$#" -ne 1 ]; then
   echo "Usage: ./run.sh num_cores"
   exit 1
 fi
-echo "======= intrinsic C code ======="
-python gflops.py -s "./tt.out" -n $1 -k
-echo "========== kernel code ========="
-python gflops.py -s "./start_avx512_a.out" -n $1 -k
+python gflops.py -s "./single_thread" -n $1 -k
