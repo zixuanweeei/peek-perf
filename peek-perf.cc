2178a7a1f8911c97b7cfa5da98290a0a1c22aa01
zixuanweeei <zixuan.wei@intel.com>
2020-08-23 22:20:22 +0800

    :page_facing_up: & :art: & :hammer: Add license, clang_format and cmake

--- peek-perf.cc
+++ peek-perf.cc
@@ -1,8 +1,25 @@
-#include <iostream>
-#include <vector>
+/*******************************************************************************
+ * Copyright 2020 zixuanweeei
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ ******************************************************************************/
+
 #include <chrono>
-#include <thread>
+#include <iostream>
 #include <mutex>
+#include <thread>
+#include <vector>
+
 #include <pthread.h>
 
 void registers_task() {
@@ -14,42 +31,40 @@ void registers_task() {
   }
   int end = 0;
 
-  asm volatile (
-    "  vmovups %1, %%zmm0 \n\t"
-    "  vmovups %2, %%zmm1 \n\t"
-    "  vmovups %1, %%zmm11 \n\t"
-    "  vmovups %2, %%zmm12 \n\t"
-    "  mov $4294967295, %%rax \n\t"
-    ".LOOP: \n\t"
-    "  vfmadd132ps %%zmm0, %%zmm0, %%zmm1 \n\t"
-    "  vfmadd132ps %%zmm1, %%zmm1, %%zmm2 \n\t"
-    "  vfmadd132ps %%zmm2, %%zmm2, %%zmm3 \n\t"
-    "  vfmadd132ps %%zmm3, %%zmm3, %%zmm4 \n\t"
-    "  vfmadd132ps %%zmm4, %%zmm4, %%zmm5 \n\t"
-    "  vfmadd132ps %%zmm5, %%zmm5, %%zmm6 \n\t"
-    "  vfmadd132ps %%zmm6, %%zmm6, %%zmm7 \n\t"
-    "  vfmadd132ps %%zmm7, %%zmm7, %%zmm8 \n\t"
-    "  vfmadd132ps %%zmm8, %%zmm8, %%zmm9 \n\t"
-    "  vfmadd132ps %%zmm9, %%zmm9, %%zmm10 \n\t"
-    "  vfmadd132ps %%zmm11, %%zmm11, %%zmm12 \n\t"
-    "  vfmadd132ps %%zmm12, %%zmm12, %%zmm13 \n\t"
-    "  vfmadd132ps %%zmm13, %%zmm13, %%zmm14 \n\t"
-    "  vfmadd132ps %%zmm14, %%zmm14, %%zmm15 \n\t"
-    "  vfmadd132ps %%zmm15, %%zmm15, %%zmm16 \n\t"
-    "  vfmadd132ps %%zmm16, %%zmm16, %%zmm17 \n\t"
-    "  vfmadd132ps %%zmm17, %%zmm17, %%zmm18 \n\t"
-    "  vfmadd132ps %%zmm18, %%zmm18, %%zmm19 \n\t"
-    "  vfmadd132ps %%zmm19, %%zmm19, %%zmm20 \n\t"
-    "  vfmadd132ps %%zmm20, %%zmm20, %%zmm21 \n\t"
-    "  sub $0x1, %%rax \n\t"
-    "  jne .LOOP \n\t"
-    "  movw $0x0F, %0"
-    : "=m" (end)
-    : "m" (a), "m" (b)
-  );
+  asm volatile("  vmovups %1, %%zmm0 \n\t"
+               "  vmovups %2, %%zmm1 \n\t"
+               "  vmovups %1, %%zmm11 \n\t"
+               "  vmovups %2, %%zmm12 \n\t"
+               "  mov $4294967295, %%rax \n\t"
+               ".LOOP: \n\t"
+               "  vfmadd132ps %%zmm0, %%zmm0, %%zmm1 \n\t"
+               "  vfmadd132ps %%zmm1, %%zmm1, %%zmm2 \n\t"
+               "  vfmadd132ps %%zmm2, %%zmm2, %%zmm3 \n\t"
+               "  vfmadd132ps %%zmm3, %%zmm3, %%zmm4 \n\t"
+               "  vfmadd132ps %%zmm4, %%zmm4, %%zmm5 \n\t"
+               "  vfmadd132ps %%zmm5, %%zmm5, %%zmm6 \n\t"
+               "  vfmadd132ps %%zmm6, %%zmm6, %%zmm7 \n\t"
+               "  vfmadd132ps %%zmm7, %%zmm7, %%zmm8 \n\t"
+               "  vfmadd132ps %%zmm8, %%zmm8, %%zmm9 \n\t"
+               "  vfmadd132ps %%zmm9, %%zmm9, %%zmm10 \n\t"
+               "  vfmadd132ps %%zmm11, %%zmm11, %%zmm12 \n\t"
+               "  vfmadd132ps %%zmm12, %%zmm12, %%zmm13 \n\t"
+               "  vfmadd132ps %%zmm13, %%zmm13, %%zmm14 \n\t"
+               "  vfmadd132ps %%zmm14, %%zmm14, %%zmm15 \n\t"
+               "  vfmadd132ps %%zmm15, %%zmm15, %%zmm16 \n\t"
+               "  vfmadd132ps %%zmm16, %%zmm16, %%zmm17 \n\t"
+               "  vfmadd132ps %%zmm17, %%zmm17, %%zmm18 \n\t"
+               "  vfmadd132ps %%zmm18, %%zmm18, %%zmm19 \n\t"
+               "  vfmadd132ps %%zmm19, %%zmm19, %%zmm20 \n\t"
+               "  vfmadd132ps %%zmm20, %%zmm20, %%zmm21 \n\t"
+               "  sub $0x1, %%rax \n\t"
+               "  jne .LOOP \n\t"
+               "  movw $0x0F, %0"
+               : "=m"(end)
+               : "m"(a), "m"(b));
 }
 
-int main(int argv, char** argc) {
+int main(int argv, char **argc) {
   int num_cores = 1;
   bool set_affinity = true;
   if (argv == 1) {
@@ -74,7 +89,7 @@ int main(int argv, char** argc) {
   std::mutex iomutex;
   auto begin = std::chrono::high_resolution_clock::now();
   for (int i = 0; i < num_cores; ++i) {
-    threads[i] = std::thread([&, i]{
+    threads[i] = std::thread([&, i] {
       registers_task();
       std::lock_guard<std::mutex> iolock(iomutex);
       std::cout << "Thread #" << i << ": on CPU " << sched_getcpu() << " ended." << std::endl;
@@ -90,14 +105,15 @@ int main(int argv, char** argc) {
     }
   }
 
-  for (auto& t : threads) {
+  for (auto &t : threads) {
     t.join();
   }
 
   auto end = std::chrono::high_resolution_clock::now();
   auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
   double gflops = 4294967295 * 160.0 * 2.0 * 2.0 / duration.count() * 1.0e-6 * num_cores;
-  std::cout << "Cost " << duration.count() << " ms, " << "GFlops: " << gflops << std::endl;
+  std::cout << "Cost " << duration.count() << " ms, "
+            << "GFlops: " << gflops << std::endl;
 
   return 0;
 }
--- single_thread.c
+++ single_thread.c
@@ -1,6 +1,22 @@
+/*******************************************************************************
+ * Copyright 2020 zixuanweeei
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ ******************************************************************************/
+
+#include <immintrin.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <immintrin.h>
 
 int main() {
   float a[16];
@@ -14,39 +30,37 @@ int main() {
   // __m512 __a = _mm512_load_ps(a);
   // __m512 __b = _mm512_load_ps(b);
 
-  asm volatile (
-    "  vmovups %1, %%zmm0 \n\t"
-    "  vmovups %2, %%zmm1 \n\t"
-    "  vmovups %1, %%zmm11 \n\t"
-    "  vmovups %2, %%zmm12 \n\t"
-    "  mov $4294967295, %%rax \n\t"
-    ".LOOP: \n\t"
-    "  vfmadd132ps %%zmm0, %%zmm0, %%zmm1 \n\t"
-    "  vfmadd132ps %%zmm1, %%zmm1, %%zmm2 \n\t"
-    "  vfmadd132ps %%zmm2, %%zmm2, %%zmm3 \n\t"
-    "  vfmadd132ps %%zmm3, %%zmm3, %%zmm4 \n\t"
-    "  vfmadd132ps %%zmm4, %%zmm4, %%zmm5 \n\t"
-    "  vfmadd132ps %%zmm5, %%zmm5, %%zmm6 \n\t"
-    "  vfmadd132ps %%zmm6, %%zmm6, %%zmm7 \n\t"
-    "  vfmadd132ps %%zmm7, %%zmm7, %%zmm8 \n\t"
-    "  vfmadd132ps %%zmm8, %%zmm8, %%zmm9 \n\t"
-    "  vfmadd132ps %%zmm9, %%zmm9, %%zmm10 \n\t"
-    "  vfmadd132ps %%zmm11, %%zmm11, %%zmm12 \n\t"
-    "  vfmadd132ps %%zmm12, %%zmm12, %%zmm13 \n\t"
-    "  vfmadd132ps %%zmm13, %%zmm13, %%zmm14 \n\t"
-    "  vfmadd132ps %%zmm14, %%zmm14, %%zmm15 \n\t"
-    "  vfmadd132ps %%zmm15, %%zmm15, %%zmm16 \n\t"
-    "  vfmadd132ps %%zmm16, %%zmm16, %%zmm17 \n\t"
-    "  vfmadd132ps %%zmm17, %%zmm17, %%zmm18 \n\t"
-    "  vfmadd132ps %%zmm18, %%zmm18, %%zmm19 \n\t"
-    "  vfmadd132ps %%zmm19, %%zmm19, %%zmm20 \n\t"
-    "  vfmadd132ps %%zmm20, %%zmm20, %%zmm21 \n\t"
-    "  sub $0x1, %%rax \n\t"
-    "  jne .LOOP \n\t"
-    "  movw $0x0F, %0"
-    : "=m" (end)
-    : "m" (a), "m" (b)
-  );
+  asm volatile("  vmovups %1, %%zmm0 \n\t"
+               "  vmovups %2, %%zmm1 \n\t"
+               "  vmovups %1, %%zmm11 \n\t"
+               "  vmovups %2, %%zmm12 \n\t"
+               "  mov $4294967295, %%rax \n\t"
+               ".LOOP: \n\t"
+               "  vfmadd132ps %%zmm0, %%zmm0, %%zmm1 \n\t"
+               "  vfmadd132ps %%zmm1, %%zmm1, %%zmm2 \n\t"
+               "  vfmadd132ps %%zmm2, %%zmm2, %%zmm3 \n\t"
+               "  vfmadd132ps %%zmm3, %%zmm3, %%zmm4 \n\t"
+               "  vfmadd132ps %%zmm4, %%zmm4, %%zmm5 \n\t"
+               "  vfmadd132ps %%zmm5, %%zmm5, %%zmm6 \n\t"
+               "  vfmadd132ps %%zmm6, %%zmm6, %%zmm7 \n\t"
+               "  vfmadd132ps %%zmm7, %%zmm7, %%zmm8 \n\t"
+               "  vfmadd132ps %%zmm8, %%zmm8, %%zmm9 \n\t"
+               "  vfmadd132ps %%zmm9, %%zmm9, %%zmm10 \n\t"
+               "  vfmadd132ps %%zmm11, %%zmm11, %%zmm12 \n\t"
+               "  vfmadd132ps %%zmm12, %%zmm12, %%zmm13 \n\t"
+               "  vfmadd132ps %%zmm13, %%zmm13, %%zmm14 \n\t"
+               "  vfmadd132ps %%zmm14, %%zmm14, %%zmm15 \n\t"
+               "  vfmadd132ps %%zmm15, %%zmm15, %%zmm16 \n\t"
+               "  vfmadd132ps %%zmm16, %%zmm16, %%zmm17 \n\t"
+               "  vfmadd132ps %%zmm17, %%zmm17, %%zmm18 \n\t"
+               "  vfmadd132ps %%zmm18, %%zmm18, %%zmm19 \n\t"
+               "  vfmadd132ps %%zmm19, %%zmm19, %%zmm20 \n\t"
+               "  vfmadd132ps %%zmm20, %%zmm20, %%zmm21 \n\t"
+               "  sub $0x1, %%rax \n\t"
+               "  jne .LOOP \n\t"
+               "  movw $0x0F, %0"
+               : "=m"(end)
+               : "m"(a), "m"(b));
   printf("end is %d\n", end);
 
   return 0;
